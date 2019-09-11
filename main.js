3070dd1ec0932fb26fbf73f741b0399773c4e297
Zoe Ames <zoeames@gmail.com>
2019-09-10 19:29:25 -0500

    added employees array and printToDom

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hi');
+const employees = [
+  {name: "Zoe", title: "Wizard", imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81Mm7PSz8qL._SX425_.jpg"},
+  {name: "Greg", title: "Lizard", imageUrl: "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_960,f_auto/Pachycephalosaurus_right_mch8y6.jpg"},
+  {name: "Michael", title: "Cool Kid", imageUrl: "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/Archaeopteryx_hmie72.png"}
+];
+
+const printToDom = (divId, textToPrint) => {
+  document.getElementById(divId).innerHTML = textToPrint;
+};
+
+const employeeStringBuilder = (ninjas)  => {
+
+};
